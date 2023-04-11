accb2f9942b5b619fa2a4111ec1b2a8fa131efd6
Franco Ojeda <fojeda@slingr.io>
2023-04-11 15:40:47 -0300

    test

--- scripts/testLib.js
+++ scripts/testLib.js
@@ -1,26 +1,42 @@
 (function() {
-    function Lodash() {
-        if (!(this instanceof Lodash)) {
-            return new Lodash();
+    function lodash() {
+        if (!(this instanceof lodash)) {
+            return new lodash();
         }else{
             return this;
         }
     }
 
-    Lodash.prototype.size = function () {
-        return "size";
+    lodash.prototype.size = function (collection) {
+        var length = collection ? collection.length : 0;
+        return typeof length == 'number' ? length : keys(collection).length;
     }
+
+
+    var keys = !nativeKeys ? shimKeys : function(object) {
+        if (!isObject(object)) {
+            return [];
+        }
+        if ((hasEnumPrototype && typeof object == 'function') ||
+            (nonEnumArgs && object.length && isArguments(object))) {
+            return shimKeys(object);
+        }
+        return nativeKeys(object);
+    };
+
+
+
 
     if (typeof exports !== 'undefined') {
         if (typeof module !== 'undefined' && module.exports) {
-            exports = module.exports = Lodash;
+            exports = module.exports = lodash;
         }
-        exports.Lodash = Lodash;
+        exports.lodash = lodash;
     }
 
     if (typeof window === "object" && typeof window.document === "object") {
-        window.Lodash = Lodash;
-        window.lodash = new Lodash();
+        window.lodash = lodash;
+        window.lodash = new lodash();
     }
 
 }());
