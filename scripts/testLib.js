47f926782ec9ca8478157aa23fc8a2a60ff5668b
Franco Ojeda <fojeda@slingr.io>
2023-04-11 16:09:19 -0300

    test

--- scripts/testLib.js
+++ scripts/testLib.js
@@ -1,14 +1,27 @@
 (function() {
-    function lodash() {
-        if (!(this instanceof lodash)) {
-            return new lodash();
-        }else{
-            return this;
+    // function lodash() {
+    //     if (!(this instanceof lodash)) {
+    //         return new lodash();
+    //     }else{
+    //         return this;
+    //     }
+    // }
+
+    function lodash(value) {
+        // exit early if already wrapped, even if wrapped by a different `lodash` constructor
+        if (value && typeof value == 'object' && value.__wrapped__) {
+            return value;
         }
+        // allow invoking `lodash` without the `new` operator
+        if (!(this instanceof lodash)) {
+            return new lodash(value);
+        }
+        this.__wrapped__ = value;
     }
+
     lodash.prototype.size = function () {
 
-        return Math.max(2,3);
+        return Math.max;
     }
     if (typeof exports !== 'undefined') {
         if (typeof module !== 'undefined' && module.exports) {
