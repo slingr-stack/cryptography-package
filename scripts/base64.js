acf44edbf2efcc0944ca0b7788743c9505f25a27
Franco Ojeda <fojeda@slingr.io>
2023-05-10 09:39:26 -0300

    config test

--- scripts/base64.js
+++ scripts/base64.js
@@ -6,17 +6,6 @@ exports.decode = function (str) {
     return sys.utils.base64.decode(str);
 }
 
-exports.dependency = function(str) {
-    var key = dependencies.testdep.test.t3();
-    return key;
-}
-
-exports.dependency2 = function(str) {
-    var key = dependencies.packageTest.test.something();
-    return key;
-}
-
-exports.nestedtest = function(){
-    var value =  "Dependency Crypto\n" + dependencies.testdep.test.doublenested()
-    return value;
+exports.configTest = function () {
+    return config.get("webhooksSharedKey");
 }
