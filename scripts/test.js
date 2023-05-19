130d398de921dc8882001e817187449004770fe8
Emanuel Montañez <emontanez@slingr.io>
2023-05-19 16:35:50 -0300

    fix

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,5 @@
 const _lodashInstance = pkg.crypto.testLib.lodash()
 
-exports.size = function () {
-    return _lodashInstance.size();
-};
-
 exports.dependencyPackage = function () {
     return dependencies.serviceTest.http.serviceTest2();
 };
