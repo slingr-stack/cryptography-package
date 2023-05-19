e568138b6b01ad6717d1f452abd7d25f85217b1b
Emanuel Montañez <emontanez@slingr.io>
2023-05-19 15:26:44 -0300

    update svc test dep

--- scripts/test.js
+++ scripts/test.js
@@ -3,3 +3,7 @@ const _lodashInstance = pkg.crypto.testLib.lodash()
 exports.size = function () {
     return _lodashInstance.size();
 };
+
+exports.dependencyPackage = function () {
+    return dependencies.serviceTest.http.serviceTest2();
+};
